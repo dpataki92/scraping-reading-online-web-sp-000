d9bdf4e3ec3aa1b3c1a4b9cda3df6c3d597ae300
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-03 01:45:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 doc.css(".headline-26OIBN")
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
